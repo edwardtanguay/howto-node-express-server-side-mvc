9412c3d6e6e1f1b3622ba5247bc9653b21b0f60a
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-19 01:46:29 +0200

    uppercase

--- src/models.js
+++ src/models.js
@@ -1,7 +1,7 @@
 import nouns from './dataSources/nouns_apiCall.js';
 import books from './dataSources/books_localJsonFile.js';
 import translations from './dataSources/translations_excelFile.js';
-import jobs from './dataSources/jobs_mdfiles.js';
+import jobs from './dataSources/jobs_mdFiles.js';
 
 export const siteData = {
     nouns,
