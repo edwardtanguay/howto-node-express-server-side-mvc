85adb74f81b1436758493e53ff64541d45224395
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-19 16:26:57 +0200

    showing slash

--- src/qtools/qfil.js
+++ src/qtools/qfil.js
@@ -4,47 +4,46 @@ import * as qfil from './qfil.js';
 import * as qsys from './qsys.js';
 import * as config from './config.js';
 import path from 'path';
-import { platform } from 'node:process';
+import { platform } from 'node:process'; // "win32" or "linux"
 
 const __dirname = path.resolve(path.dirname(''));
 
-console.log(`platform is: ${platform}`);
+export const getSiteRelativePathAndFileNames = (folderPath) => {
+    let result = [];
+    let fileNamesInPath = fs.readdirSync(folderPath);
 
-// export const getSiteRelativePathAndFileNames = (folderPath) => {
-//     let result = [];
-//     let fileNamesInPath = fs.readdirSync(folderPath);
-
-//     const osSlash = platform === 'win32' ? '\\' : '/';
-//     fileNamesInPath.forEach((fileName) => {
-//         let filePath = folderPath + osSlash + fileName;
-//         if (!fs.statSync(filePath).isDirectory()) {
-//             let filePath = folderPath + osSlash + fileName;
-//             result.push(filePath);
-//         }
-//     });
-//     return result;
-// };
-
-export const getSiteRelativePathAndFileNames = function (absoluteDirectory, files_) {
-    absoluteDirectory = absoluteDirectory || __dirname;
-    files_ = files_ || [];
-    var files = fs.readdirSync(absoluteDirectory);
-    const osSlash = qsys.getOperatingSystemSlash();
-    for (var i in files) {
-        var absolutePathAndFileName = absoluteDirectory + osSlash + files[i];
-        if (!qstr.contains(absolutePathAndFileName, '\\node_modules\\')) {
-            absolutePathAndFileName = qstr.replaceAll(absolutePathAndFileName, `\\\\`, `\\`);
-            if (fs.statSync(absolutePathAndFileName).isDirectory()) {
-                qfil.getSiteRelativePathAndFileNames(absolutePathAndFileName, files_);
-            } else {
-                const relativePathAndFileName = qfil.getRelativePathAndFileName(absolutePathAndFileName);
-                const fixedPathAndFileName = qfil.convertBackSlashesToForwardSlashes(relativePathAndFileName);
-                files_.push(fixedPathAndFileName);
-            }
+    const osSlash = platform === 'win32' ? '\\' : '/';
+    console.log(osSlash);
+    fileNamesInPath.forEach((fileName) => {
+        let filePath = folderPath + osSlash + fileName;
+        if (!fs.statSync(filePath).isDirectory()) {
+            let filePath = folderPath + osSlash + fileName;
+            result.push(filePath);
         }
-    }
-    return files_;
-}
+    });
+    return result;
+};
+
+// export const getSiteRelativePathAndFileNames = function (absoluteDirectory, files_) {
+//     absoluteDirectory = absoluteDirectory || __dirname;
+//     files_ = files_ || [];
+//     var files = fs.readdirSync(absoluteDirectory);
+//     const osSlash = qsys.getOperatingSystemSlash();
+//     for (var i in files) {
+//         var absolutePathAndFileName = absoluteDirectory + osSlash + files[i];
+//         if (!qstr.contains(absolutePathAndFileName, '\\node_modules\\')) {
+//             absolutePathAndFileName = qstr.replaceAll(absolutePathAndFileName, `\\\\`, `\\`);
+//             if (fs.statSync(absolutePathAndFileName).isDirectory()) {
+//                 qfil.getSiteRelativePathAndFileNames(absolutePathAndFileName, files_);
+//             } else {
+//                 const relativePathAndFileName = qfil.getRelativePathAndFileName(absolutePathAndFileName);
+//                 const fixedPathAndFileName = qfil.convertBackSlashesToForwardSlashes(relativePathAndFileName);
+//                 files_.push(fixedPathAndFileName);
+//             }
+//         }
+//     }
+//     return files_;
+// }
 
 export const getRelativePathAndFileName = function (absolutePathAndFileName) {
     return qstr.chopLeft(absolutePathAndFileName, __dirname);
