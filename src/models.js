db3793a6c4ea7d7c786141839a1378313c83a1a3
Edward Tanguay (windows) <edward@tanguay.info>
2022-06-20 05:42:42 +0200

    showing nicely

--- src/models.js
+++ src/models.js
@@ -2,10 +2,12 @@ import nouns from './dataSources/nouns_apiCall.js';
 import books from './dataSources/books_localJsonFile.js';
 import translations from './dataSources/translations_excelFile.js';
 import jobs from './dataSources/jobs_mdFiles.js';
+import landscapePhotos from './dataSources/images_landscapePhotos.js';
 
-export const siteData = { 
+export const siteData = {
     nouns,
     books,
     translations,
-    jobs
+    jobs,
+    landscapePhotos
 };
--- src/views.js
+++ src/views.js
@@ -1,4 +1,10 @@
-export const siteView = ({ nouns, books, translations, jobs }) => {
+export const siteView = ({
+    nouns,
+    books,
+    translations,
+    jobs,
+    landscapePhotos
+}) => {
     return `
 <!DOCTYPE html>
 <html lang="en">
@@ -14,24 +20,55 @@ export const siteView = ({ nouns, books, translations, jobs }) => {
 
     <h2><b>API call: </b>There are ${nouns.length} nouns.</h2>
     <div class="content">
-        ${nouns.filter((m,i) => i < 3).map(m => m.article + ' ' + m.singular).join(', ')}, ...
+        ${nouns
+            .filter((m, i) => i < 3)
+            .map((m) => m.article + ' ' + m.singular)
+            .join(', ')}, ...
     </div>
 
     <h2><b>Local JSON file: </b>There are ${books.length} books.</h2>	
     <div class="content">
-        ${books.filter((m,i) => i < 3).map(m => m.title).join(', ')}, ...
+        ${books
+            .filter((m, i) => i < 3)
+            .map((m) => m.title)
+            .join(', ')}, ...
     </div>
 
-    <h2><b>Local Excel file: </b>There are ${translations.length} translations.</h2>	
+    <h2><b>Local Excel file: </b>There are ${
+        translations.length
+    } translations.</h2>	
     <div class="content">
-        ${translations.filter((m,i) => i < 3).map(m => m.fromPhrase).join(', ')}, ...
+        ${translations
+            .filter((m, i) => i < 3)
+            .map((m) => m.fromPhrase)
+            .join(', ')}, ...
     </div>
 
-    <h2><b>Local directory of markdown files: </b>There are ${jobs.length} jobs.</h2>
+    <h2><b>Local directory of images: </b>There are ${
+        landscapePhotos.length
+    } landscape photos.</h2>
     <div class="content">
-        ID-CODE: ${jobs[0].idCode}
-        ${jobs[0].html}
+        ${landscapePhotos
+            .map((fileName) => `<img src="images/${fileName}"/>`)
+            .join('')}
     </div>
+
+    <h2><b>Local directory of markdown files: </b>There are ${
+        jobs.length
+    } jobs.</h2>
+    <div class="content">
+        ${jobs
+            .map((job) => {
+                return `
+                <div class="job">
+                    ID-CODE: ${job.idCode}
+                    ${job.html}
+                </div>
+                `;
+            })
+            .join('')}
+    </div>
+
 </body>
 </html>
 `;
